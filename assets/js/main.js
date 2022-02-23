2d5b9bfe24e59d51057441c3ff4592e2443f98d8
s4m5apu3l <asperatuz@yandex.ru>
2022-02-23 23:29:52 +0900

    feat: add scroll animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,43 @@ const swiper = new Swiper(".my-swiper", {
   },
 });
 
-console.log('true')
+console.log('true');
+
+// animation scroll
+
+const anime = document.querySelectorAll('.animation');
+
+if (anime.length > 0) {
+  window.addEventListener('scroll', animeScroll);
+
+  function animeScroll() {
+  for (let index = 0; index < anime.length; index++) {
+    const animeItem = anime[index];
+    const animeItemHeight = animeItem.offsetHeight;
+    const animeItemOffset = offset(animeItem).top;
+    const animeStart = 4;
+
+    let animePoint = window.innerHeight - animeItemHeight / animeStart;
+
+      if(animeItemHeight  > window.innerHeight) {
+        animePoint = window.innerHeight - window.innerHeight / animeStart;
+      }
+
+      if((pageYOffset > animeItemOffset - animePoint) && pageYOffset < (animeItemOffset + animeItemHeight)) {
+        animeItem.classList.add('active');
+
+      } else {
+        if(!animeItem.classList.contains('anime-no-hide')) {
+          animeItem.classList.remove('active');
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+  }
+  animeScroll();
+}
