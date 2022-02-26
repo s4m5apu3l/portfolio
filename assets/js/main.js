fd8be87cb4adfddef334556c53e8375a48d830ef
s4m5apu3l <asperatuz@yandex.ru>
2022-02-26 22:57:38 +0900

    feat: add responsive add experience mob version section

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,3 +59,28 @@ if (anime.length > 0) {
   }
   animeScroll();
 }
+
+// MENU BURGER 
+
+const burger = document.querySelector('.header__burger-btn');
+const menu = document.querySelector('.header-nav__list');
+const body = document.body;
+
+let menuOpen = false;
+
+burger.addEventListener('click', () => {
+  if(!menuOpen) {
+    burger.classList.toggle('opened');
+    burger.setAttribute('aria-expanded', burger.classList.contains('opened'));
+    body.classList.toggle('noscroll')
+    menu.classList.toggle('active');
+  }
+});
+
+function closeMenu() {
+  menu.classList.remove('active');
+  burger.classList.remove('opened');
+  body.classList.remove('noscroll')
+}
+const navLinks = document.querySelectorAll('.link');
+navLinks.forEach((el) => el.addEventListener('click', closeMenu,));
